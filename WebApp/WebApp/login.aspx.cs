45cb441bd3f7e8e2c06210ee5d599de851e4bde4
shehan <shehan.peruma@gmail.com>
2017-11-26 00:50:41 +0000

    inital code commit

---
+++ WebApp/WebApp/login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            if (FormsAuthentication.Authenticate(UserName.Text, UserPass.Text))
+            {
+               // FormsAuthentication.SetAuthCookie(UserName.Text, false);
+                FormsAuthentication.RedirectFromLoginPage(UserName.Text, true);
+                //FormsAuthentication.RedirectFromLoginPage(UserName.Text, chkboxPersist.Checked);
+            }
+            else
+            {
+                Msg.Text = "Invalid User Name and/or Password";
+            }
+        }
+    }
+}
